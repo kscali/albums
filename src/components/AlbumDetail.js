00e6bea8233a76c272161a98844625e62313e83b
koy <kosatun@aol.com>
2019-06-17 22:27:44 -0700

    created reusable button component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, Linking } from "react-native";
 import Card from "./Card";
 import CardSection from "./CardSection";
+import Button from "./Button";
 
 const AlbumDetail = ({ album }) => {
-  const { artist, thumbnail_image, title, image } = album;
+  const { artist, thumbnail_image, title, image, url } = album;
   const {
     thumbnailStyle,
     headerContentStyle,
@@ -24,8 +25,13 @@ const AlbumDetail = ({ album }) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>Buy Now</Button>
       </CardSection>
     </Card>
   );
